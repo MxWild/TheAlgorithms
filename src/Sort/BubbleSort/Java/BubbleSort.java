5f07446d484f5de7f09f6974e8c8ec8f149d4eda
MxWild <mxwild@gmail.com>
2018-11-24 22:12:35 +0300

    Create bubble sort on java

--- src/Sort/BubbleSort/Java/BubbleSort.java
+++ src/Sort/BubbleSort/Java/BubbleSort.java
@@ -1,4 +1,34 @@
 package Sort.BubbleSort.Java;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+  private static int arr[] = {12, 54, 98, 45, 32, 85, 9, 122, 2};
+
+
+  public static void main(String[] args) {
+    bubbleSort(arr);
+    System.out.println("Sorted array is: ");
+    printArray(arr);
+  }
+
+  private static void bubbleSort(int[] arr) {
+
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = 0; j < arr.length - 1 - i; j++) {
+        if(arr[j] > arr[j + 1]) {
+          int tmp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = tmp;
+        }
+      }
+
+    }
+  }
+
+  private static void printArray(int[] arr) {
+    System.out.println(Arrays.toString(arr));
+  }
+
 }
