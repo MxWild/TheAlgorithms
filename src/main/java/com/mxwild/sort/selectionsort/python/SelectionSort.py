06135a86c17127ea3910ed67ca08e7e1ff1d4f14
MxWild <mxwild@gmail.com>
2018-11-26 16:21:45 +0300

    Add travis

--- src/main/java/com/mxwild/sort/selectionsort/python/SelectionSort.py
+++ src/main/java/com/mxwild/sort/selectionsort/python/SelectionSort.py
@@ -1,6 +1,5 @@
 def selection_sort(array):
-
-    for i in range(len(array)-1, 0, -1):
+    for i in range(len(array) - 1, 0, -1):
         positionOfMax = 0
 
         for loc in range(1, i + 1):
@@ -10,7 +9,6 @@ def selection_sort(array):
         temp = array[i]
         array[i] = array[positionOfMax]
         array[positionOfMax] = temp
-
 
 
 arr = [12, 43, 27, -10, 49, 15, 15]
